23db42a731dea5f00c796c143d84ac07670c7554
Alla <yefremova.alla@gmail.com>
2022-01-14 12:17:13 +0300

    init: done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+  const arrNumb = String(n).split('').filter(el => el !== '-')
+  const reverseNumb = arrNumb.reverse().join('')
+  return reverseNumb
 }
